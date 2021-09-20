04ad22e5fd71bba69668e5f4f87be5a36187733e
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 11:14:46 +0300

    Rokas

---
+++ components/ToDoItem/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {TextInput, TouchableOpacity, View} from "react-native";
+import tw from "tailwind-react-native-classnames";
+import Checkbox from "../CheckBox";
+
+const ToDoItem = () => {
+    return (
+        <View>
+            <View style={tw`flex flex-row items-center justify-center`}>
+                {/*  Checkbox */}
+                <TouchableOpacity activeOpacity={0.8}>
+                    <View style={tw`mt-2`}>
+                        <Checkbox  onPress={() => setValue(!value)} isChecked={value} />
+                    </View>
+                </TouchableOpacity>
+
+                {/*   Text input  */}
+                <TextInput multiline style={tw`flex flex-1 text-white text-xl`} />
+            </View>
+        </View>
+    );
+};
+
+export default ToDoItem;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -6,26 +6,16 @@ import tw from "tailwind-react-native-classnames";
 import Checkbox from "../components/CheckBox";
 import {useState} from "react";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
     const [value, setValue] = useState(false);
 
   return (
     <View style={tw`flex p-5 items-center justify-center`}>
       <Text style={tw`font-bold text-2xl`}>Collab todo</Text>
 
-      <View style={tw`flex flex-row items-center justify-center`}>
-      {/*  Checkbox */}
-          <TouchableOpacity activeOpacity={0.8}>
-              <View style={tw`mt-2`}>
-                <Checkbox  onPress={() => setValue(!value)} isChecked={value} />
-              </View>
-          </TouchableOpacity>
 
-      {/*   Text input  */}
-        <TextInput style={tw`flex flex-1 text-white text-xl`} />
-      </View>
     </View>
   );
 }
 
-
+export default TabOneScreen;
