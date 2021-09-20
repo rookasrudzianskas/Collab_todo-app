8ed0da71864f7f4e9674227ded9e48df6e8c9257
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 11:01:06 +0300

    Rokas

---
+++ components/CheckBox/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from "react-native";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+
+interface CheckBoxProps {
+    isChecked: boolean,
+}
+
+const Checkbox = (props: CheckBoxProps) => {
+
+    const name = props.isChecked ? 'checkbox-marked-outline' : 'checkbox-blank-outline';
+    return (
+        <View>
+            <MaterialCommunityIcons name="checkbox-marked-outline" size={24} color="black" />
+        </View>
+    );
+};
+
+export default Checkbox;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet, TextInput} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import tw from "tailwind-react-native-classnames";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+
   return (
     <View style={tw`flex p-5 items-center justify-center`}>
-      <Text style={tw`font-bold text-xl`}>Collab todo</Text>
+      <Text style={tw`font-bold text-2xl`}>Collab todo</Text>
+
+      <View style={tw`flex flex-row items-center justify-center`}>
+      {/*  Checkbox */}
+          <MaterialCommunityIcons name="checkbox-blank-outline" size={24} color="white" />
+
+      {/*   Text input  */}
+        <TextInput style={tw`flex flex-1 text-white text-xl`} />
+      </View>
     </View>
   );
 }
