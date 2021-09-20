62dcd35f1a9da069daf5c67832f5e880a9fdbac6
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 15:05:35 +0300

    Rokas

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -6,6 +6,7 @@ import { Text, View } from '../components/Themed';
 import tw from "tailwind-react-native-classnames";
 import {AntDesign, Entypo, Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
 import {useNavigation} from "@react-navigation/native";
+import ProjectItem from "../components/ProjectItem";
 
 export default function TabTwoScreen() {
 
@@ -27,40 +28,9 @@ export default function TabTwoScreen() {
               <Text style={tw`text-gray-400`}>RECENT</Text>
             </View>
           </View>
-          {/*  tasklist */}
-          <View style={tw`flex flex-col`}>
-          <View style={tw`flex flex-row items-center p-2`}>
-            <View style={tw`bg-gray-700 p-2 rounded-xl`}>
-              <MaterialCommunityIcons name="file-outline" size={40} color="gray" />
+            <View>
+              <ProjectItem />
             </View>
-            <View style={tw`flex flex-col`}>
-              <View style={tw`flex flex-row`}>
-                <Text style={tw`text-2xl font-medium ml-2`}>Title lorem ipsum</Text>
-                <Text style={tw`ml-2 text-xl text-gray-500`}>2 h ago</Text>
-              </View>
-
-              <View style={tw`flex flex-row ml-3 mt-1 items-center`}>
-                <View style={tw`flex flex-row items-center bg-gray-600 px-2 py-1 rounded-full border border-gray-400`}>
-                  <Entypo name="browser" size={22} color="lightgray" />
-                  <Text style={tw`ml-2`}>Date</Text>
-                </View>
-
-                <View style={tw`flex flex-row items-center mx-3 bg-gray-600 px-2 py-1 rounded-full border border-gray-400`}>
-                  <Ionicons name="person" size={22} color="lightgray" />
-                  <Text style={tw`ml-2`}>Assign</Text>
-                </View>
-
-                <View style={tw`flex flex-row items-center ml-0 bg-gray-600 px-2 py-1 rounded-full border border-gray-400`}>
-                  <AntDesign name="tag" size={22} color="lightgray" />
-                  <Text style={tw`ml-2`}>Tag</Text>
-              </View>
-              </View>
-            </View>
-
-
-
-          </View>
-          </View>
         </View>
       </TouchableOpacity>
   );
