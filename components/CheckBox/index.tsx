045176578274c443b504e36c9e3af73c39a8b264
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 11:10:47 +0300

    rokas

--- components/CheckBox/index.tsx
+++ components/CheckBox/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, TouchableOpacity} from "react-native";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
+import tw from "tailwind-react-native-classnames";
 
 interface CheckBoxProps {
     isChecked: boolean,
+    onPress: () => void,
 }
 
 const Checkbox = (props: CheckBoxProps) => {
+    const {onPress, isChecked} = props;
 
-    const name = props.isChecked ? 'checkbox-marked-outline' : 'checkbox-blank-outline';
+    const name = isChecked ? 'checkbox-marked-outline' : 'checkbox-blank-outline';
     return (
-        <View>
-            <MaterialCommunityIcons name={name} size={24} color="white" />
-        </View>
+        <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+            <View style={tw`mr-2`}>
+                <MaterialCommunityIcons name={name} size={27} color="white" />
+            </View>
+        </TouchableOpacity>
     );
 };
 
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {StyleSheet, TextInput} from 'react-native';
+import {StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +7,10 @@ import { RootTabScreenProps } from '../types';
 import tw from "tailwind-react-native-classnames";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Checkbox from "../components/CheckBox";
+import {useState} from "react";
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-
+    const [value, setValue] = useState(false);
 
   return (
     <View style={tw`flex p-5 items-center justify-center`}>
@@ -17,7 +18,11 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 
       <View style={tw`flex flex-row items-center justify-center`}>
       {/*  Checkbox */}
-          <Checkbox isChecked={false} />
+          <TouchableOpacity activeOpacity={0.8}>
+              <View style={tw`mt-2`}>
+                <Checkbox  onPress={() => setValue(!value)} isChecked={value} />
+              </View>
+          </TouchableOpacity>
 
       {/*   Text input  */}
         <TextInput style={tw`flex flex-1 text-white text-xl`} />
