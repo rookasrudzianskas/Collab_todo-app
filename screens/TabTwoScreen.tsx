bc1b8f70c5d69656cbdd9c928bc4e85e49b014f8
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 14:43:35 +0300

    Rokas

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -10,12 +10,17 @@ export default function TabTwoScreen() {
   return (
     <View style={tw`mt-16`}>
 
-      <View style={tw`flex flex-row items-center`}>
-        <MaterialCommunityIcons name="chevron-left" size={35} color="yellow" />
-        <Text style={tw`flex flex-1 text-xl`}>Rokas Developments</Text>
-        <MaterialCommunityIcons style={tw`mx-1`} name="chevron-down" size={30} color="yellow" />
-        <MaterialCommunityIcons style={tw`mx-1`} name="filter-menu-outline" size={30} color="yellow" />
-        <MaterialCommunityIcons style={tw`mx-1`} name="dots-horizontal" size={30} color="yellow" />
+      <View style={tw`p-2`}>
+        <View style={tw`flex flex-row items-center`}>
+          <MaterialCommunityIcons name="chevron-left" size={35} color="yellow" />
+          <Text style={tw`flex flex-1 text-xl`}>Rokas Developments</Text>
+          <MaterialCommunityIcons style={tw`mx-1`} name="chevron-down" size={30} color="yellow" />
+          <MaterialCommunityIcons style={tw`mx-1`} name="filter-menu-outline" size={30} color="yellow" />
+          <MaterialCommunityIcons style={tw`mx-1`} name="dots-horizontal" size={30} color="yellow" />
+        </View>
+        <View style={tw`my-2 ml-2 mt-5 mb-5`}>
+          <Text style={tw`text-gray-400`}>RECENT</Text>
+        </View>
       </View>
       {/*  tasklist */}
       <View style={tw``}>
